c106c5e6174995e44c6f2447fa702d280ffccb89
Lucas Ortengren <lucasortengren@gmail.com>
2024-08-04 13:35:04 -0500

    Tuning hyperparameters

---
+++ analysis.py
@@ -0,0 +1,112 @@
+import json
+import copy
+import joblib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import keras
+import keras_tuner
+from dataclasses import dataclass
+from typing import List
+from keras import layers, ops
+from keras.utils import timeseries_dataset_from_array
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, PowerTransformer
+from sklearn.linear_model import LinearRegression
+
+from processing import Stationarizer, Normalizer
+from plotting import visualize_loss, show_plot
+from model_builder import build_model, build_spec_model, get_tuner
+
+
+def sort(array):
+    less = []
+    equal = []
+    greater = []
+    if len(array) > 1:
+        pivot = array[0]["score"]
+        for x in array:
+            if x["score"] < pivot:
+                less.append(x)
+            elif x["score"] == pivot:
+                equal.append(x)
+            elif x["score"] > pivot:
+                greater.append(x)
+        return sort(less)+equal+sort(greater)
+    else:
+        return array
+
+
+def get_trials(num_trials, proj_name):
+  trials = list()
+  for n in range(100):
+    path = f"tuning/stock_predictor/trial_{n:03d}/trial.json"
+    with open(path, mode="r") as f:
+      trial = json.load(f)
+      trials.append(trial)
+  return sort(trials)
+
+
+def get_RMSEs(trials):
+  tmp = copy.deepcopy(trials)
+  rmse_trials = list()
+  for trial in tmp:
+    trial.update({"rmse" : np.sqrt(trial["score"])})
+    rmse_trials.append(trial)
+  return rmse_trials
+
+
+def make_histogram(rmse_trials, num_bins=25, width=0.1):
+  bins = np.linspace(min([x["rmse"] for x in rmse_trials]), max([x["rmse"] for x in rmse_trials]), num_bins)
+  counts = [[cutoff, 0] for cutoff in bins]
+  t = 0
+  c = 0
+  while t < len(rmse_trials) and c < len(counts):
+    if rmse_trials[t]["score"] <= counts[c][0]:
+      counts[c][1] += 1
+      t += 1
+    else:
+      c += 1
+  scx = list()
+  heights = list()
+  for el in counts:
+    scx.append(el[0])
+    heights.append(el[1])
+
+  plt.bar(scx, heights, edgecolor="black", width=width)
+  plt.xlabel("RMSE")
+  plt.ylabel("number of occurrences")
+  plt.title("Score distribution")
+  plt.show()
+  return counts
+
+
+def plot_lrs(rmse_trials, log=False, lin_reg=None):
+  rmse_scores = np.array([trial["rmse"] for trial in rmse_trials])
+  if log:
+    lrs = np.array([np.log10(trial["hyperparameters"]["values"]["lr"]) for trial in rmse_trials])
+    xlabel = "log(learning rate)"
+  else:
+    xlabel = "learning rate"
+    lrs = np.array([trial["hyperparameters"]["values"]["lr"] for trial in rmse_trials])
+  plt.scatter(lrs, rmse_scores.reshape((-1, 1)))
+  plt.xlabel("learning rate")
+  plt.ylabel("RMSE")
+  if lin_reg:
+    reg.fit(lrs.reshape((-1, 1)), rmse_scores.reshape((-1, 1)))
+    x = np.linspace(min(lrs), max(lrs), 100)
+    y = reg.coef_ * x + reg.intercept_
+    plt.plot(x, y.reshape((-1, 1)))
+  plt.show()
+
+
+def get_trials_df(rmse_trials):
+  df = pd.DataFrame([trial["hyperparameters"]["values"] for trial in rmse_trials])
+  df["trial id"] = [trial["trial_id"] for trial in rmse_trials]
+  df["rmse"] = [trial["score"] for trial in rmse_trials]
+  df["mse"] = [trial["score"] ** 2 for trial in rmse_trials]
+  df["best step"] = [trial["best_step"] for trial in rmse_trials]
+  df["total dropout"] = df["dropout_0"] + df["dropout_1"]
+  return df
+
--- model_builder.py
+++ model_builder.py
@@ -48,20 +48,28 @@ def build_model(hp):
       batch_size=64,
       name="Inputs"
   ))  
-  model.add(layers.Dense(32, activation="relu"))
+#  units_0 = 32 #hp.Int(f"units_0", min_value=32, max_value=96, step=32)
+  if hp.Boolean("selu"):
+    model.add(layers.Dense(32, activation="selu", kernel_initializer="lecun_normal"))
+  else:
+    model.add(layers.Dense(32, activation=hp.Choice(f"activation", ["relu", "silu", "gelu"])))
   num_layers = 2 #hp.Int("num_layers", 1, 3)
   for i in range(num_layers):
+#    if i == 0:
+#      u = units_0
+#    else:
+#      u = hp.Int(f"units_{i}", min_value=32, max_value=96, step=32)
     activations = ["sigmoid", "tanh"]
-    inits = ["glorot_uniform", "he_normal"]
+    inits = ["glorot_uniform", "glorot_normal", "he_uniform", "he_normal"]
     model.add(
       layers.LSTM(
-        units=32, #hp.Int(f"units_{i}", min_value=16, max_value=96, step=16),
+        units=32, #u,
         activation="tanh", #hp.Choice(f"activation_{i}", activations),
         recurrent_activation="sigmoid",#hp.Choice(f"recurrent_activation_{i}", activations),
-        dropout=hp.Float(f"dropout_{i}", min_value=0.0, max_value=0.4, step=0.1),
-#        recurrent_dropout=0.0, #hp.Float(f"recurrent_dropout_{i}", min_value=0.0, max_value=0.6, step=0.1),
+        dropout=hp.Float(f"dropout_{i}", min_value=0.2, max_value=0.5),
+        recurrent_dropout=hp.Float(f"recurrent_dropout_{i}", min_value=0.0, max_value=0.2),
         return_sequences=(i != num_layers - 1),
-        kernel_initializer=hp.Choice(f"kernel_initializer_{i}", inits),
+        kernel_initializer="glorot_uniform", #hp.Choice(f"kernel_initializer_{i}", inits),
         recurrent_initializer="orthogonal", #hp.Choice(f"recurrent_initializer_{i}", inits),
         bias_initializer="zeros"#""hp.Choice(f"bias_initializer_{i}", inits+["zeros"])
       ))
@@ -72,7 +80,7 @@ def build_model(hp):
       kernel_initializer=hp.Choice(f"kernel_initializer_{num_layers}", inits),
       bias_initializer="zeros" #hp.Choice(f"bias_initializer_{num_layers}", inits+["zeros"]),
     ))
-  learning_rate=hp.Float("lr", min_value=1e-3, max_value=1e-2, sampling="log")
+  learning_rate=hp.Float("lr", min_value=5e-5, max_value=4e-3, sampling="log")
   model.compile(
       optimizer=keras.optimizers.Adam(learning_rate=learning_rate), #, clipnorm=1.0),
       loss="mse",
@@ -120,15 +128,18 @@ def build_spec_model(
   return model
 
 
-def get_tuner():
+def get_tuner(max_trials, execs_per_trial, project_name=None):
+  if project_name == None:
+    project_name = "stock_predictor_" + str(datetime.now())
   tuner = keras_tuner.RandomSearch(
     hypermodel=build_model,
     objective="val_mse",
-    max_trials=100,
-    executions_per_trial=1,
+    max_trials=max_trials,
+    executions_per_trial=execs_per_trial,
     overwrite=True,
     directory="tuning",
-    project_name="stock_predictor_" + str(datetime.now()),
+    project_name=project_name,
   )
   return tuner
+
 
